fe1a65073157ea3a797f0b16fa79e3f59914c56f
guilt-alt <viacheslav.savvidi@gmail.com>
2023-04-11 02:20:36 +0600

    Рендер пустого списка фильмов и рефакторинг

--- src/js/view/film-list.js
+++ src/js/view/film-list.js
@@ -1,10 +1,10 @@
 import { createElement } from '@utils/render.js';
 
-const createFilmList = () => `<section class="films">
-  <section class="films-list">
-    <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-    <div class="films-list__container js-film-list-main">
-    </div>
+const createFilmList = (data) => {
+  const noData = '<h2 class="films-list__title">There are no movies in our database</h2>';
+  const lists = `<section class="films-list">
+      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+      <div class="films-list__container js-film-list-main"></div>
   </section>
   <section class="films-list films-list--extra">
     <h2 class="films-list__title">Top rated</h2>
@@ -15,18 +15,22 @@ const createFilmList = () => `<section class="films">
     <h2 class="films-list__title">Most commented</h2>
     <div class="films-list__container js-film-list-commented">
     </div>
-  </section>
-</section>`;
+  </section>`;
+
+  return `<section class="films">${data ? lists : noData}</section >`;
+};
 
 export default class FilmListView {
+  #data = null;
+
   #element = null;
 
-  constructor() {
-    this.createFilmList = createFilmList();
+  constructor(data) {
+    this.#data = data;
   }
 
   getTemplate() {
-    return this.createFilmList;
+    return createFilmList(this.#data);
   }
 
   getElement() {
